c8eb9ec0ffa90c032dd5d73f9f8221d99b599599
Mubaraq Allamalyekeen <mubaraqadeniyi159@gmail.com>
2025-07-22 15:36:35 +0100

    last

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,9 +34,9 @@ app.use("/api/expense", authMiddleware, expenseRoute)
 app.use("/api/dashboard", authMiddleware, dashRoute)
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("files{/*path}",(req, res)=>{
-        res.sendFile(path.join(__dirname,"../client","dist","index.html"))
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     });
 };
 
